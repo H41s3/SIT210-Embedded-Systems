efda3be1a4542c5220f3ddecb72337bff6946c77
EJ <115877407+H41s3@users.noreply.github.com>
2026-03-18 20:37:04 +1100

    Update 1.1Lights_ON.ino

--- task1/1.1Lights_ON.ino
+++ task1/1.1Lights_ON.ino
@@ -1,27 +1,37 @@
-#define BUTTON_PIN 2   // button on pin D2 (same as their diagram)
-#define PORCH_LED  5   // porch LED on pin D5
-#define HALLWAY_LED 3  // hallway LED on pin D3 (add second LED here)
+#define BUTTON_PIN  2
+#define PORCH_LED   5
+#define HALLWAY_LED 3
 
-void setup() {
+void setupPins() {
   pinMode(PORCH_LED,   OUTPUT);
   pinMode(HALLWAY_LED, OUTPUT);
   pinMode(BUTTON_PIN,  INPUT_PULLUP);
 }
 
+bool isButtonPressed() {
+  return digitalRead(BUTTON_PIN) == LOW;
+}
+
+void activatePorchLight() {
+  digitalWrite(PORCH_LED, HIGH);
+  delay(30000);
+  digitalWrite(PORCH_LED, LOW);
+}
+
+void activateHallwayLight() {
+  digitalWrite(HALLWAY_LED, HIGH);
+  delay(60000);
+  digitalWrite(HALLWAY_LED, LOW);
+}
+
+void setup() {
+  setupPins();
+}
+
 void loop() {
-  int button_state = digitalRead(BUTTON_PIN);
-
-  if (button_state == LOW) {   // button pressed once
-    delay(50);                 // debounce
-
-    // Porch light ON for 30 seconds
-    digitalWrite(PORCH_LED, HIGH);
-    delay(30000);
-    digitalWrite(PORCH_LED, LOW);
-
-    // Hallway light ON for 60 seconds
-    digitalWrite(HALLWAY_LED, HIGH);
-    delay(60000);
-    digitalWrite(HALLWAY_LED, LOW);
+  if (isButtonPressed()) {
+    delay(50);
+    activatePorchLight();
+    activateHallwayLight();
   }
 }
